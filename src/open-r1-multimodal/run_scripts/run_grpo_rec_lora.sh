7892cc70fefc594a3e3a6a48f9a4496122405f2c
SabaPivot <careforme.dropout@gmail.com>
2025-03-28 09:16:26 +0000

    Add: DAPO features

--- src/open-r1-multimodal/run_scripts/run_grpo_rec_lora.sh
+++ src/open-r1-multimodal/run_scripts/run_grpo_rec_lora.sh
@@ -1,12 +1,15 @@
-cd src/open-r1-multimodal
+# cd /workspace/VLM-R1-DAPO
+# bash setup.sh
+
+cd /workspace/VLM-R1-DAPO/src/open-r1-multimodal
 
 export DEBUG_MODE="true"
-# export CUDA_VISIBLE_DEVICES=4,5,6,7
-
-RUN_NAME="Qwen2.5-VL-7B-GRPO-REC-lora"
+export CUDA_VISIBLE_DEVICES=1,2
+RUN_NAME="Qwen2.5-VL-3B-GRPO-REC-lora-SFT-10992steps-continuous-iou"
 export LOG_PATH="./debug_log_$RUN_NAME.txt"
 
-torchrun --nproc_per_node="8" \
+
+torchrun --nproc_per_node="2" \
     --nnodes="1" \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
@@ -14,13 +17,13 @@ torchrun --nproc_per_node="8" \
     src/open_r1/grpo_rec.py \
     --deepspeed local_scripts/zero2.json \
     --output_dir output/$RUN_NAME \
-    --model_name_or_path Qwen/Qwen2.5-VL-7B-Instruct \
+    --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
     --dataset_name data_config/rec.yaml \
-    --image_root <your_image_root> \
+    --image_root /workspace/data/NIH_CXR \
     --max_prompt_length 1024 \
-    --num_generations 8 \
+    --num_generations 2 \
     --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 4 \
     --logging_steps 1 \
     --bf16 \
     --torch_dtype bfloat16 \
@@ -38,6 +41,7 @@ torchrun --nproc_per_node="8" \
     --lora_alpha 128 \
     --lora_dropout 0.05 \
     --lora_task_type CAUSAL_LM \
-    --freeze_vision_modules true
-
+    --freeze_vision_modules true \
+    --beta 0.0 \
+    --epsilon_high 0.28
 
